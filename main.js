2e81f375d05a3ba6ab3b965c2a1985f7f1d50af9
Sheel Patel <115194165+Kyoko522@users.noreply.github.com>
2023-07-24 00:01:44 -0400

    Music was added and the Three.js stars were optimized

--- main.js
+++ main.js
@@ -16,21 +16,24 @@ let delayTimer = 1000; // Delay duration in milliseconds
 let elapsedTime = 0;
 let modelFollowsCursor = false;
 
-const scene = new THREE.Scene();
+//Creating a new Scene
+const scene = new THREE.Scene();//creating a new scene
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+// Setting up the Camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);//setting up the camera
+camera.position.setZ(30);
+camera.position.setX(-3);
 
-const renderer = new THREE.WebGLRenderer({
+// Creating a new renderer
+const renderer = new THREE.WebGLRenderer({//creating a new renderer
   canvas: document.querySelector('#bg'),
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
-camera.position.setX(-3);
-
 renderer.render(scene, camera);
 
+//Function to load a 3D model with animation
 function loadModelWithAnimation(loader, modelPath, position, scale, rotationSpeed) {
   loader.load(modelPath, function (gltf) {
     const model = gltf.scene;
@@ -50,41 +53,38 @@ function loadModelWithAnimation(loader, modelPath, position, scale, rotationSpee
   });
 }
 
-const anime_model = new GLTFLoader();
-const anime_model2 = new GLTFLoader();
+const yamato = new GLTFLoader();
+const raphtalia = new GLTFLoader();
 const chopper = new GLTFLoader();
+const rocket_model = new GLTFLoader();
 
 loadModelWithAnimation(
-  anime_model,
+  yamato,
   'yamato.gltf',
-  new THREE.Vector3(-29.98, -27.4, 12),
-  0.07,
+  new THREE.Vector3(-29.98, -27.4, 12), 
+  0.07, 
   0.05
 );
 
 loadModelWithAnimation(
-  anime_model2,
+  raphtalia,
   'anime2.gltf',
-  new THREE.Vector3(-39.98, -29.4, 14),
-  0.068,
+  new THREE.Vector3(-39.98, -29.4, 14), 
+  0.068, 
   -0.05
 );
 
 loadModelWithAnimation(
   chopper,
   'chopper.gltf',
-  new THREE.Vector3(-52.98, -26.4, 16),
-  0.068,
+  new THREE.Vector3(-52.98, -26.4, 16), 
+  0.068, 
   0.05
 );
 
-
-//Rocket
-const rocket_model = new GLTFLoader();
-
 rocket_model.load('model.gltf', function (gltf) {
   const rocket = gltf.scene;
-  scene.add(rocket);
+  // scene.add(rocket);//removing the rocket for now will added it later when i get the movement right
 
   // Set initial rotation
   rocket.rotation.y = Math.PI / 2; // Adjust the angle as needed
@@ -94,53 +94,12 @@ rocket_model.load('model.gltf', function (gltf) {
     readerComponent.matrix.copy(entity.worldMatrix);
   }
 
-  // const pursuer = new YUKA.Vehicle();
-  // pursuer.setRenderComponent(rocket, sync);
-  // YUKA.EntityManager.add(pursuer);
-  // pursuer.position.set(-10, 0, -3);
-  // pursuer.maxSpeed = 3;
-
-  // const evaderGeometry = new THREE.SphereBufferGeometry(0.01);
-  // const evaderMaterial = new THREE.MeshPhongMaterial({ color: 0xFFEA00 });
-  // const evaderMesh = new THREE.Mesh(evaderGeometry, evaderMaterial);
-  // evaderMesh.matrixAutoUpdate = false;
-  // scene.add(evaderMesh);
-
-  // const evader = new YUKA.Vehicle();
-  // evader.setRenderComponent(evaderMesh, sync);
-  // entityManager.add(evader);
-  // evader.position.set(2, 0, -3);
-  // evader.maxSpeed = 2;
-
-  // const pursuitBehavior = new YUKA.PursuitBehavior(evader, 5);
-  // pursuer.steering.add(pursuitBehavior);
-
-  // const evaderTarget = new YUKA.Vector3();
-  // const seekBehavior = new YUKA.SeekBehavior(evaderTarget);
-  // evader.steering.add(seekBehavior);
-
-  // const time = new YUKA.Time();
-
-
-  // // Animation function to rotate the model
-  // function animate() {
-  //   const delta = time.update().getDelta();
-  //   YUKA.entityManager.update(delta);
-
-  //   const elapsed = time.getElapsed();
-  //   evaderTarget.x = Math.cos(elapsed) * Math.sin(elapsed * 0.2) * 6;
-  //   evaderTarget.z = Math.sin(elapsed * 0.8) * 6;
-
-  //   renderer.render(scene, camera);
-  //   requestAnimationFrame(animate);
-  // }
-
   // animate();
 }, undefined, function (error) {
   console.error("The model failed to load:", error);
 });
 
-// Torus
+// Torus's
 const geometry = new THREE.TorusGeometry(6, 0.87, 30, 1000,);
 const material = new THREE.MeshStandardMaterial({ color: "#36013F" });
 // const material = new THREE.MeshStandardMaterial({ color: "#A020F0" });
@@ -159,9 +118,7 @@ const material3 = new THREE.MeshStandardMaterial({ color: "#000000" })
 const torus3 = new THREE.Mesh(geometry3, material3);
 torus3.position.set(3.0, 0, -4)
 
-scene.add(torus);
-scene.add(torus2);
-scene.add(torus3);
+scene.add(torus, torus2, torus3);
 
 // Lights
 const pointLight = new THREE.PointLight(0xffffff);
@@ -170,82 +127,41 @@ pointLight.position.set(5, 5, 5);
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
-// Stars
+// Function to add stars
+const starGeometry = new THREE.IcosahedronGeometry(0.2, 0);//creating a new Icosahedron object to represent the stars
+
+// Define the colors for the stars
+const starColors = [
+  "#e200f7", // pink stars
+  "#014760", // dark/navy blue stars
+  "#25f5fc", // bright blue stars
+  "#ffffff", // white stars
+  "#34fe4f", // green stars
+  "#d1fe49", // yellow stars
+  "#FF5F1F", // orange stars
+  "#FF3131"  // red stars
+];
+
 function addStar() {
-  const geometry = new THREE.IcosahedronGeometry(0.2, 0);
-  //dark or navy blue stars 
-  const material2 = new THREE.MeshStandardMaterial({ color: "#014760" });
-  //pink stars
-  const material = new THREE.MeshStandardMaterial({ color: "#e200f7" });
-  //bright blue stars
-  const material3 = new THREE.MeshStandardMaterial({ color: "#25f5fc" });
-  //white stars
-  const material4 = new THREE.MeshStandardMaterial({ color: "#ffffff" });
-  //green stars
-  const material5 = new THREE.MeshStandardMaterial({ color: "#34fe4f" });
-  //yellow stars
-  const material6 = new THREE.MeshStandardMaterial({ color: "#d1fe49" });
-  //orange stars
-  const material7 = new THREE.MeshStandardMaterial({ color: "#FF5F1F" });
-  //red stars
-  const material8 = new THREE.MeshStandardMaterial({ color: "#FF3131" });
+  // Pick a random color for the star
+  const randomColor = starColors[Math.floor(Math.random() * starColors.length)];
+  const material = new THREE.MeshStandardMaterial({ color: randomColor });
 
-  const star = new THREE.Mesh(geometry, material);
-  const star2 = new THREE.Mesh(geometry, material2);
-  const star3 = new THREE.Mesh(geometry, material3);
-  const star4 = new THREE.Mesh(geometry, material4);
-  const star5 = new THREE.Mesh(geometry, material5);
-  const star6 = new THREE.Mesh(geometry, material6);
-  const star7 = new THREE.Mesh(geometry, material7);
-  const star8 = new THREE.Mesh(geometry, material8);
+  // Create the star mesh using the shared geometry and random color material
+  const star = new THREE.Mesh(starGeometry, material);
 
-  const [x1, y1, z1] = Array(3)
-    .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(350));
+  // Generate random positions for stars once
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(600));
+  star.position.set(x, y, z);
 
-  const [x2, y2, z2] = Array(3)
-    .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(350));
-
-  const [x3, y3, z3] = Array(3)
-    .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(350));
-
-  const [x4, y4, z4] = Array(3)
-    .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(350));
-
-  const [x5, y5, z5] = Array(3)
-    .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(350));
-
-  const [x6, y6, z6] = Array(3)
-    .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(350));
-
-  const [x7, y7, z7] = Array(3)
-    .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(350));
-
-  const [x8, y8, z8] = Array(3)
-    .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(350));
-
-
-  star.position.set(x1, y1, z1);
-  star2.position.set(x2, y2, z2);
-  star3.position.set(x3, y3, z3);
-  star4.position.set(x4, y4, z4);
-  star5.position.set(x5, y5, z5);
-  star6.position.set(x6, y6, z6);
-  star7.position.set(x7, y7, z7);
-  star8.position.set(x8, y8, z8);
-
-  scene.add(star, star2, star3, star4, star5, star6, star7, star8);
-
+  scene.add(star);
 }
-Array(9000).fill().forEach(addStar);
 
+// Define the number of stars you want to add to the scene
+const numStars = 99999; // You can adjust this number as needed
+
+// Adding stars to the scene
+Array(numStars).fill().forEach(addStar);
 
 // Background
 const spaceTexture = new THREE.TextureLoader();
@@ -258,15 +174,15 @@ const sheelTexture3 = new THREE.TextureLoader().load('sheel3.png'); //Adding a p
 
 const cubematerial = [
   new THREE.MeshBasicMaterial({ map: sheelTexture }), //this one is upside down
-  new THREE.MeshBasicMaterial({ map: sheelTexture }),
+  new THREE.MeshBasicMaterial({ map: sheelTexture }), //more images will or should be added later need 3 more images so that each face is different
   new THREE.MeshBasicMaterial({ map: sheelTexture2 }),
   new THREE.MeshBasicMaterial({ map: sheelTexture2 }),
   new THREE.MeshBasicMaterial({ map: sheelTexture3 }),
   new THREE.MeshBasicMaterial({ map: sheelTexture3 })
 ]
-const sheel = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), cubematerial);
+const sheel_box = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), cubematerial);
 
-scene.add(sheel);
+scene.add(sheel_box);
 
 // Earth
 const earthTexture = new THREE.TextureLoader().load('earth.jpg');
@@ -282,12 +198,13 @@ const earth = new THREE.Mesh(
 
 scene.add(earth);
 
-earth.position.z = 80;
+
 earth.position.x = -30;
+earth.position.z = 80;
 earth.position.y = 1.55;
 
-sheel.position.z = -5;
-sheel.position.x = 2;
+sheel_box.position.z = -5;
+sheel_box.position.x = 2;
 
 
 // Sun
@@ -303,9 +220,24 @@ const sun = new THREE.Mesh(
 
 scene.add(sun);
 
-sun.position.z = 92
-sun.position.x = 1
-sun.position.y = -60
+
+sun.position.x = -150
+sun.position.z = 0
+sun.position.y = -10
+
+//create a blue LineBasicMaterial
+const helpers = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+
+const points = [];
+points.push( new THREE.Vector3( 0, 0, 100 ) );
+points.push( new THREE.Vector3( 0, 100, 0 ) );
+points.push( new THREE.Vector3( 100, 0, 0 ) );
+
+const lines = new THREE.BufferGeometry().setFromPoints( points );
+
+const line = new THREE.Line( lines, helpers );
+
+scene.add( line );
 
 
 // Scroll Animation
@@ -313,13 +245,10 @@ function moveCamera() {
 
   console.log(camera.position)
   const t = document.body.getBoundingClientRect().top;
-  // earth.rotation.x += 1.5;
-  // earth.rotation.y += 0.075;
-  // earth.rotation.z += 0.05;
 
-  sheel.rotation.y += 0.05;
+  sheel_box.rotation.y += 0.05;
 
-  sheel.rotation.z += 0.05;
+  sheel_box.rotation.z += 0.05;
 
   camera.position.z = t * -0.04;
   camera.position.x = t * -0.0004;
@@ -352,28 +281,26 @@ function animate() {
   }
 
 // to go back to the original rotation cahnge the first 1 into a 0
-  torus.rotation.x -= 1.11865367;
-  torus.rotation.y -= 1.119367;
-  torus.rotation.z -= 1.4354678;
+  torus.rotation.x -= 0.91865367;
+  torus.rotation.y -= 0.919367;
+  torus.rotation.z -= 0.9354678;
 
-  torus2.rotation.x += 1.08312087;
-  torus2.rotation.y += 1.082093;
-  torus2.rotation.z += 1.073049856;
+  torus2.rotation.x += 0.08312087;
+  torus2.rotation.y += 0.082093;
+  torus2.rotation.z += 0.073049856;
 
-  torus3.rotation.x -= 1.02;
-  torus3.rotation.y += 1.01;
-  torus3.rotation.z -= 1.02;
+  torus3.rotation.x -= 0.02;
+  torus3.rotation.y += 0.01;
+  torus3.rotation.z -= 0.02;
 
   earth.rotation.y += 0.05;
   earth.rotation.x += 0.001;
 
-  sheel.rotation.y += 0.05;
-  sheel.rotation.z += 0.02;
+  sheel_box.rotation.y += 0.05;
+  sheel_box.rotation.z += 0.02;
 
   renderer.render(scene, camera);
 }
-
-
 
 const loop = () => {
   renderer.render(scene, camera);
---
+++ music.js
@@ -0,0 +1,7 @@
+window.addEventListener('click',() =>{
+    document.getElementById("song").play();
+  })
+
+window.addEventListener('scroll',() =>{
+    document.getElementById("song").play();
+});

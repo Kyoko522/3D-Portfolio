25e11727a134001102be269591474c9b11332ae9
Sheel Patel <115194165+Kyoko522@users.noreply.github.com>
2023-07-24 00:27:17 -0400

    Music added

--- music.js
+++ music.js
@@ -2,6 +2,9 @@ window.addEventListener('click',() =>{
     document.getElementById("song").play();
   })
 
+  myaudio=document.getElementById("song");
+myaudio.playbackRate=2.0;
+
 window.addEventListener('scroll',() =>{
     document.getElementById("song").play();
 });
